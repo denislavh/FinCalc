9998777a5b8c5ccdfebb35903760bdd8e7c78ad2
Denislav Vladimirov <denislav.hristov@gmail.com>
2024-02-27 00:37:09 +0200

    Add project files.

---
+++ FinCalc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinCalc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
